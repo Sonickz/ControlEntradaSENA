e17dddd41ffc4bea54b95a988be9a88f25831e1c
Giovanny Ladino <ladigiococ@gmail.com>
2023-08-07 21:22:10 -0500

    ControlEntradaSENA V2.0.1: Organizacion carpeta static

--- ControlEntradaSENA/settings.py
+++ ControlEntradaSENA/settings.py
@@ -130,17 +130,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "mainapp/static"),
-    os.path.join(BASE_DIR, "administrator/static"),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 
 #Imagenes
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
